6fffdcfd91a005f919933872dc137887ce41b64c
benjobas <166581907+benjobas@users.noreply.github.com>
2024-08-24 00:04:35 -0400

    V2

--- app.js
+++ app.js
@@ -5,39 +5,29 @@ const mongoose = require('mongoose')
 const morgan = require('morgan')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-const blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 
-app.use(express.json())
-app.use(cors())
 
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-
-
-const Blog = mongoose.model('Blog', blog.blogSchema)
-
+mongoose.set('strictQuery', false)
 mongoose.connect(config.MONGODB_URL)
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
---
+++ controllers/blogs.js
@@ -0,0 +1,46 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            console.log(blogs)
+            response.json(blogs)
+        })
+})
+
+blogsRouter.get('/:id', (request, response) => {
+    Blog
+        .findById(request.params.id)
+        .then(blog => {
+            if (blog) {
+                response.json(blog)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+blogsRouter.post('/', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+        .catch(error => next(error))
+})
+
+blogsRouter.delete('/:id', (request, response, next) => {
+    Blog
+        .findByIdAndDelete(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+module.exports = blogsRouter
