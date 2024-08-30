e0ae8559c2ee8c269b5a6436d0f58cebf87da4d8
benjobas <166581907+benjobas@users.noreply.github.com>
2024-08-30 16:55:57 -0400

    Finally working sending .rest

--- app.js
+++ app.js
@@ -28,8 +28,10 @@ app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({});
@@ -12,15 +13,27 @@ blogsRouter.get("/:id", async (request, response) => {
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const { title, url } = request.body;
+  const { title, author, url, likes, userId} = request.body;
+  const user = await User.findById(userId);
 
   if (!title || !url) {
     return response.status(400).json({ error: "Title or URL is missing" });
-  } else {
-  const blog = new Blog(request.body);
-  const savedBlog = await blog.save();
-  response.status(201).json(savedBlog);
+  } 
+  if (!user) {
+    return response.status(400).json({ error: "User not found" });
   }
+  
+    const blog = new Blog({
+      title: title,
+      author: author,
+      url: url,
+      likes: likes === undefined ? 0 : likes,
+      user: user.id
+    })
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save();
+    response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
